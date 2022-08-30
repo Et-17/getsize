76df318a0a1654564dc3dc64a8115b812a931c54
Luna Kilborn <leland.kilborn@hotmail.com>
2022-08-29 20:07:29 -0400

    Macroitize args file
    I noticed that the flag detection functions all had basically the exact same structure, so I made a macro to both increase readability and conciseness.

--- src/args.c
+++ src/args.c
@@ -3,38 +3,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int raw_flag(int argc, char **argv)
-{
-	for (int i = 0; i < argc; i++)
-	{
-		if (strcmp(argv[i], RAW_FLAG) == 0)
-		{
-			return 1;
-		}
+#define FLAG_FUNC(n, f)                          \
+	int n(int argc, char **argv)             \
+	{                                        \
+		for (int i = 0; i < argc; i++)   \
+			if (!strcmp(argv[i], f)) \
+				return 1;        \
+		return 0;                        \
 	}
-	return 0;
-}
 
-int x_flag(int argc, char **argv)
-{
-	for (int i = 0; i < argc; i++)
-	{
-		if (strcmp(argv[i], X_FLAG) == 0)
-		{
-			return 1;
-		}
-	}
-	return 0;
-}
-
-int y_flag(int argc, char **argv)
-{
-	for (int i = 0; i < argc; i++)
-	{
-		if (strcmp(argv[i], Y_FLAG) == 0)
-		{
-			return 1;
-		}
-	}
-	return 0;
-}
+FLAG_FUNC(raw_flag, RAW_FLAG);
+FLAG_FUNC(x_flag, X_FLAG);
+FLAG_FUNC(y_flag, Y_FLAG);
